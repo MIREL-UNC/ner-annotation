e9c2adfe9b66e62a4f0a7c3e0ce9b08be41019d9
Milagro Teruel <milagro.teruel@gmail.com>
2016-11-09 14:11:29 -0300

    Refactoring code to ignore new project

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+# If no configuration, use the templates
+if [[ ! -e config/folders.txt ]]; then
+    cp config/folders.txt.template config/folders.txt;
+fi;
+if [[ ! -e config/labels.txt ]]; then
+    cp config/labels.txt.template config/labels.txt;
+fi;
 mvn spring-boot:run -Drun.addResources=true -Dserver.port=8080
