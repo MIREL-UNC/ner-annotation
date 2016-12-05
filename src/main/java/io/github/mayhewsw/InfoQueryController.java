f9629ef2fb50b7310e3e113d874da45ac49b3cca
Milagro Teruel <milagro.teruel@gmail.com>
2016-12-05 09:14:30 -0300

    Query dbpedia for description

--- src/main/java/io/github/mayhewsw/InfoQueryController.java
+++ src/main/java/io/github/mayhewsw/InfoQueryController.java
@@ -1,6 +1,8 @@
 package io.github.mayhewsw;
 
 import org.apache.jena.query.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
 
 
 /**
@@ -18,13 +22,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class InfoQueryController {
 
     private static Logger logger = LoggerFactory.getLogger(AnnotationController.class);
+    private final String URI_TYPE = "uri";
+    private final String CLASS_TYPE = "class";
+    private final String DBPEDIA_ENDPOINT = "http://dbpedia.org/sparql";
+    private final String YAGO_ENDPOINT = "https://linkeddata1.calcul.u-psud.fr/sparql";
+    private ConfigurationManager config;
 
-    public InfoQueryController() {
-        logger.info("Creating controller");
+    public InfoQueryController() throws IOException, ParseException {
+        config = new ConfigurationManager("config/config.json");
     }
 
-    @RequestMapping(value = "/description", method = RequestMethod.GET)
-    public String getDescription(@RequestParam(value = "labelName") String labelName, Model model) {
+    private String getEntityDescription(String labelName) {
         logger.info("Getting description " + labelName);
         String queryString = "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
                 "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
@@ -35,15 +43,43 @@ public class InfoQueryController {
                 "     FILTER (langMatches(lang(?description),\"en\"))\n" +
                 "} LIMIT 1";
         Query query = QueryFactory.create(queryString);
-        ResultSet results = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query).execSelect();
+        ResultSet results = QueryExecutionFactory.sparqlService(DBPEDIA_ENDPOINT, query).execSelect();
         String description = "";
         if (results.hasNext()) {
             QuerySolution solution = results.next();
             description = solution.getLiteral("?description").toString();
         }
-        model.addAttribute("entity", labelName);
-        model.addAttribute("infoString", description);
+        return  description;
+    }
 
+    private String getClassGloss(String labelName) {
+        String gloss = "";
+        return gloss;
+    }
+
+    @RequestMapping(value = "/description", method = RequestMethod.GET)
+    public String getInfo(@RequestParam(value = "labelValue") String labelValue,
+                          @RequestParam(value = "labelName") String labelName, Model model) {
+        String description;
+        String labelType;
+        try {
+            labelType = ((JSONObject) config.getLabelInfoQueryType()).get(labelName).toString();
+        } catch (NullPointerException e) {
+            logger.warn("Error, unmapped label type " + labelName);
+            model.addAttribute("errorString", "Error, unmapped label type" + labelName);
+            return "info";
+        }
+        if (labelType.equals(URI_TYPE)) {
+            description = getEntityDescription(labelValue);
+        } else if (labelType.equals(CLASS_TYPE)) {
+            description = getClassGloss(labelValue);
+        } else {
+            description = "Error: no description";
+            logger.warn("Error, unknown label type " + labelName);
+        }
+
+        model.addAttribute("entity", labelValue);
+        model.addAttribute("infoString", description);
         return "info";
     }
 }
